bfa2093aa003abb413eccf3a7dffdc085aa4cff9
Quildreen Motta <quildreen@gmail.com>
2018-10-27 19:38:08 -0300

    Copies the runtime to the output dir

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 RUNTIME_FILES := $(wildcard source/runtime/*.js)
 
 .PHONY: all
-all: build/fohm-runtime.js
+all: runtime/fohm-runtime.js
 
-build/fohm-runtime.js: $(RUNTIME_FILES)
+runtime/fohm-runtime.js: $(RUNTIME_FILES)
 	mkdir -p $(dir $@)
 	./node_modules/.bin/browserify -s Fohm source/runtime/index.js > $@
 
--- source/bin/fohm.js
+++ source/bin/fohm.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const fs = require("fs");
 const path = require("path");
+const mkdirp = require("mkdirp");
 const { inspect } = require("util");
 const fohm = require("../");
 
@@ -18,17 +19,45 @@ function show(data) {
   return inspect(data, false, null, true);
 }
 
+/**
+ * The path to the runtime file.
+ */
+const runtimePath = path.join(__dirname, "../../runtime/fohm-runtime.js");
+
 // -- CLI --
 require("yargs")
   .command("ast <file>", "shows the ast for <file>", {}, argv => {
     const program = read(argv.file);
     console.log(show(fohm.parse(program)));
   })
-  .command("compile <file>", "compiles <file> to F#", {}, argv => {
-    const program = read(argv.file);
-    const ast = fohm.parse(program);
-    console.log(fohm.generate(ast));
-  })
+  .command(
+    "compile <file>",
+    "compiles <file> to F#",
+    {
+      name: {
+        description: "The name of the file to generate.",
+        required: true
+      },
+      outDir: {
+        description: "The directory to place the generated files.",
+        required: true
+      }
+    },
+    argv => {
+      const program = read(argv.file);
+
+      console.info(`-> Compiling ${argv.file}`);
+      mkdirp.sync(argv.outDir);
+      const ast = fohm.parse(program);
+      fs.writeFileSync(
+        path.join(argv.outDir, argv.name + ".fs"),
+        fohm.generate(ast)
+      );
+
+      console.info(`-> Copying Fohm's runtime to ${argv.outDir}`);
+      fs.copyFileSync(runtimePath, path.join(argv.outDir, "fohm-runtime.js"));
+    }
+  )
   .help()
   .strict()
   .demandCommand(1).argv;

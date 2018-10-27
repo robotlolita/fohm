86076ec2b691290c7611567eab41999c068407bb
Quildreen Motta <quildreen@gmail.com>
2018-10-27 14:48:03 -0300

    Fixes a bunch of things about the example

--- examples/lisp/src/Program.fs
+++ examples/lisp/src/Program.fs
@@ -1,5 +1,11 @@
-﻿module Lisp
+﻿module LispProgram
 
 open Lisp.Grammar
 open Lisp.Syntax
 
+let program = """
+  (define one 1)
+  (f (lambda [x y] (plus x y)) one)
+"""
+
+printfn "%A" (Lisp.parse program)
--- examples/lisp/src/Syntax.fs
+++ examples/lisp/src/Syntax.fs
@@ -1,9 +1,9 @@
-module Lisp.Syntax
+namespace Lisp.Syntax
 
 type LispExpr = 
   | List of exprs: LispExpr list
   | Define of name: string * value: LispExpr
   | Call of head: LispExpr * args: LispExpr list
-  | Lambda of params: string list * body: LispExpr list
+  | Lambda of param: string list * body: LispExpr list
   | Symbol of name: string
   | Number of name: int
